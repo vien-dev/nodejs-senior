908757809f8c1f1be759f0c49bd5b47e6cf32f0c
Vien Mai <56462929+vien-dev@users.noreply.github.com>
2023-06-27 04:12:33 +0200

    Add role field to customer in DB
    Role field has value from an enum ('USER' or 'ADMIN')

--- prisma/seeds/customers.ts
+++ prisma/seeds/customers.ts
@@ -11,4 +11,10 @@ export const customers: Prisma.CustomerUpsertArgs['create'][] = [
     email: 'user2@gmail.com',
     password: 'randow-password',
   },
+  {
+    id: '9e391faf-64b2-4d4c-b879-463532920fd1',
+    email: 'admin@interview.volvo.com',
+    password: 'randow-password',
+    role: 'ADMIN'
+  }
 ];
